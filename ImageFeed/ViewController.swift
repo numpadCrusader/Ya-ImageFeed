79397dbb11cc8a3509129036bfe7fa5472a57e08
n_khon <khonikitos@gmail.com>
2025-02-06 22:06:19 +0500

    initial commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Nikita Khon on 06.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
