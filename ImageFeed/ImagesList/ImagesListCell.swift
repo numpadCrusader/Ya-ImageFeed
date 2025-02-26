a4a592c55858f4e2909dc061b3ae32af34686835
n_khon <khonikitos@gmail.com>
2025-02-26 23:27:53 +0500

    added table view and custom cell

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Nikita Khon on 09.02.2025.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ImagesListViewController.swift
+//  ImageFeed
+//
+//  Created by Nikita Khon on 06.02.2025.
+//
+
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = 200
+    }
+    
+    func configCell(for cell: ImagesListCell) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imageListCell)
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
