e27213a041bdcb7f8f5c0d0b84b5fa32e747cd95
n_khon <khonikitos@gmail.com>
2025-03-11 23:50:59 +0500

    did basic layout for profile view controller

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  ImageFeed
+//
+//  Created by Nikita Khon on 11.03.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet var statusLabel: UILabel!
+    @IBOutlet var nicknameLabel: UILabel!
+    @IBOutlet var fullNameLabel: UILabel!
+    @IBOutlet var avatarImage: UIImageView!
+    
+    // MARK: - IBAction
+    
+    @IBAction func didTapLogOutButton(_ sender: Any) {
+        // TODO: - Добавить логику при нажатии на кнопку
+    }
+}
