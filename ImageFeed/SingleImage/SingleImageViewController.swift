1981d8fbfff5dbf5e8539b2b7cc3f342fe0b4606
n_khon <khonikitos@gmail.com>
2025-03-13 23:31:17 +0500

    added SingleImageViewController and route to it

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Nikita Khon on 13.03.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet var singleImageView: UIImageView!
+}
