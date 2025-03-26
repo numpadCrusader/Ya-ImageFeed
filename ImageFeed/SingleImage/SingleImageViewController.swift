e88b18116b30e059d5ffe54e271fe292aeb2462a
n_khon <nikita.gamersax@gmail.com>
2025-03-26 13:33:56 +0500

    adjusted ui by following review comments
    removed like button in `SingleImageViewController`; set cell selection style to `none`.

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -45,10 +45,6 @@ final class SingleImageViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func didTapLikeButton(_ sender: Any) {
-        // TODO: - Добавить логику при нажатии на кнопку
-    }
-    
     @IBAction func didTapShareButton(_ sender: Any) {
         guard let image else { return }
         let activityVC = UIActivityViewController(activityItems: [image], applicationActivities: nil)
