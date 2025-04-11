99e9557175b0668dfd01843072977b984e94c907
n_khon <khonikitos@gmail.com>
2025-04-11 22:18:02 +0500

    updated usage of tab bar controller

---
+++ ImageFeed/ViewControllers/TabBarController/TabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarController.swift
+//  ImageFeed
+//
+//  Created by Nikita Khon on 11.04.2025.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    // MARK: - UITabBarController
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        let profileViewController = makeProfileViewController()
+        
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
+    
+    // MARK: - Private Methods
+    
+    private func makeProfileViewController() -> UIViewController {
+        let profileViewController = ProfileViewController()
+        
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil)
+        
+        return profileViewController
+    }
+}
